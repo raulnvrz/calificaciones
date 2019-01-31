333e00b1ee77002c762f9273a256647c1472fa05
raulnvrz <rnevarez@quantumbit.mx>
2019-01-30 19:35:25 -0700

    Se agregaron ultimos detalles del sistema.

--- class/calificaciones.class.php
+++ class/calificaciones.class.php
@@ -57,34 +57,88 @@ class Calificaciones {
            return $grades;
     }
 
-    public function get_grades_average(){
-        $grades_average = array();
+    public function get_average($type="grades"){
 
-        foreach($this->get_grades() as $val){
+        if($type=="grades"){
+            $total_average = array();
+
+            foreach($this->get_grades() as $val){
+                $p=0; // Promedios sumados
+                $i=0; // Numero de promedios por grado
+                $grade = array();
+            
+                foreach($this->data() as $key){
+                    if($val == $key[3]){
+                        $p = $p + $key[5];
+                        $i = $i + 1;
+                    }
+                }
+    
+                    $average = $p / $i;
+    
+                    $grade = array("grade"=>$val,"average"=>$average);
+    
+                    array_push($total_average, $grade);
+            }
+        }elseif($type=="total"){
+            $total_average = 0;
+
             $p=0; // Promedios sumados
             $i=0; // Numero de promedios por grado
-            $grade = array();
         
             foreach($this->data() as $key){
-                if($val == $key[3]){
                     $p = $p + $key[5];
                     $i = $i + 1;
-                }
             }
 
-                $average = $p / $i;
+              $total_average = $average = $p / $i;
 
-                $grade = array("grade"=>$val,"average"=>$average);
-
-                array_push($grades_average, $grade);
         }
 
-
-        return $grades_average;
+        return $total_average;
     }
 
     public function get_general_average(){
 
+    }
+
+    public function get_max_min($type="max"){
+        $data = $this->data();
+        $ratings = array();
+
+        if($type == "max"){
+    
+            $max = 0;
+            foreach( $data as $k => $v )
+            { 
+                $max = max( array( $max, $v[5] ) );
+                if($v[5] == $max){
+                    $better = array("rating"=>$max, "student"=>$v[0].' '.$v[1].' '.$v[2]);
+                    array_push($ratings, $better);
+                }
+            }
+        }elseif($type == "min"){
+            $lowestKey = 10;
+
+            foreach($data as $item)
+            {
+                if($item[5] < $lowestKey)
+                {
+                    $lowestKey = $item[5];
+                    unset($ratings);
+                    $ratings = array();
+                    $worst = array("rating"=>$lowestKey, "student"=>$item[0].' '.$item[1].' '.$item[2]);
+                    array_push($ratings, $worst);
+                }elseif($item[5] == $lowestKey){
+                    
+                    $worst = array("rating"=>$lowestKey, "student"=>$item[0].' '.$item[1].' '.$item[2]);
+                    array_push($ratings, $worst);
+                }
+            
+            }
+        }
+
+        return $ratings;
     }
 
 }
--- index.php
+++ index.php
@@ -49,18 +49,33 @@
 
                     <?php 
                       if(isset($_FILES['excelfile'])){
-                      
+              
+                        $mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+                        if(in_array($_FILES["excelfile"]["type"],$mimes)){
+
+                        include 'class/simplexlsx.class.php';
+                        include 'class/calificaciones.class.php';
+
+                          $xlsx = new SimpleXLSX( $_FILES['excelfile']['tmp_name'] );//Instanciamos la clase y le pasamos el parametro del archivo excel
+              
+                          $alumnos = new Calificaciones($xlsx);
                     ?>
               
-              <!-- Earnings (Monthly) Card Example -->
               <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-success shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Mejor calificación</div>
-                        <div class="text-xs font-weight-bold text-default mb-1">Mejor calificación</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                        <?php
+                        foreach($alumnos->get_max_min("max") as $val){
+                          print '<div class="text-xs font-weight-bold text-default mb-1">'.$val["student"].'</div>';
+                          $best_rating = $val["rating"];
+                        }
+
+                        ?>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?php print $best_rating; ?></div>
                       </div>
 
                     </div>
@@ -68,15 +83,20 @@
                 </div>
               </div>
   
-              <!-- Earnings (Monthly) Card Example -->
               <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-warning shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Calificación más baja</div>
-                        <div class="text-xs font-weight-bold text-default mb-1">Mejor calificación</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                        <?php
+                        foreach($alumnos->get_max_min("min") as $val){
+                          print '<div class="text-xs font-weight-bold text-default mb-1">'.$val["student"].'</div>';
+                          $worst_rating = $val["rating"];
+                        }
+
+                        ?>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?php print $worst_rating; ?></div>
                       </div>
 
                     </div>
@@ -92,7 +112,7 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Promedio general</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?php print number_format($alumnos->get_average("total"),1) ?></div>
                       </div>
 
                     </div>
@@ -104,17 +124,6 @@
           <div class="row">
 
           <div class="col-lg-6 mb-4">
-
-          <?php 
-          include 'class/simplexlsx.class.php';
-          include 'class/calificaciones.class.php';
-
-          if(isset($_FILES['excelfile'])){
-            $xlsx = new SimpleXLSX( $_FILES['excelfile']['tmp_name'] );//Instanciamos la clase y le pasamos el parametro del archivo excel
-
-            $alumnos = new Calificaciones($xlsx);
-
-          ?>
 
             <!-- Alumns Card -->
                    <!-- <div class="col-xl-8 col-lg-7"> -->
@@ -138,8 +147,7 @@
           </div> 
 
           <?php
-            print '<textarea id="data_chart" style="display:none;">'.json_encode($alumnos->get_grades_average()).'</textarea>';
-          }
+            print '<textarea id="data_chart" style="display:none;">'.json_encode($alumnos->get_average("grades")).'</textarea>';
 
           ?>
 
@@ -167,20 +175,36 @@
           </div>
 
           <?php
-              }else{
+
+                  }else{
+
+                    ?>
+                <div class="col-lg-6 mb-4">
+                  <div class="card bg-warning text-white shadow">
+                    <div class="card-body">
+                      El archivo cargado no es un excel.
+                      <!-- <div class="text-white-50 small">#f6c23e</div> -->
+                    </div>
+                  </div>
+                </div>
+                    <?php
+
+                  }
+
+              }else{ // PRINCIPAL ELSE
 
           ?>
                               <!-- Illustrations -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Ejercicio</h6>
                 </div>
                 <div class="card-body">
                   <div class="text-center">
                     <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
                   </div>
-                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-                  <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
+                  <p>Sistema para carga de excel con calificaciones, para el muestreo de calificaciones en graficas, obtención de promedio por grado, mejor calificación, peor calificación y el promedio total.</p>
+                  <p>- Cargar el archivo xlsx en la parte superior para obtener los resultados.</p>
                 </div>
               </div>
 
